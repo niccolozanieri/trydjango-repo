aeef0fa01627d6289b1bf881574623eda68540f8
Nick <niccolo.zanieri.13@gmail.com>
2021-04-17 12:49:20 +0200

    Added Article model and run migrations

--- src/blog/models.py
+++ src/blog/models.py
@@ -3,12 +3,12 @@ from django.db import models
 # Create your models here.
 
 
-# class Article(models.Model):
-#     author = models.CharField(max_length=40, default='Anonymous')
-#     text = models.TextField(blank=True, null=True)
-#     title = models.CharField(max_length=50)
-#     likes = models.PositiveIntegerField(default=0)
-#     dislikes = models.PositiveIntegerField(default=0)
+class Article(models.Model):
+    author = models.CharField(max_length=40, default='Anonymous')
+    text = models.TextField(blank=True, null=True)
+    title = models.CharField(max_length=50)
+    likes = models.PositiveIntegerField(default=0)
+    dislikes = models.PositiveIntegerField(default=0)
 
 
 
