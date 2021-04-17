a12a2cb9c0e452eaa532a61476df04c66255a5ae
Nick <niccolo.zanieri.13@gmail.com>
2021-04-17 12:48:45 +0200

    Added blog app

--- src/blog/views.py
+++ src/blog/views.py
@@ -1,23 +1,3 @@
-from django.shortcuts import get_object_or_404, render
+from django.shortcuts import render
 
-from django.views.generic import (
-    CreateView,
-    DetailView,
-    ListView,
-    UpdateView,
-    DeleteView
-)
-
-from .models import Article
 # Create your views here.
-
-class ArticleListView(ListView):
-    template_name = 'articles/articles_list.html'
-    queryset = Article.objects.all()
-
-class ArticleDetailView(DetailView):
-    template_name = 'articles/articles_detail.html'
-
-    def get_object(self):
-        id_ = self.kwargs.get("id")
-        return get_object_or_404(Article, id= id_)
