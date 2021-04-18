db3cfeae9704bd1963bf8c7f62654b5b1cb05b2a
Nick <niccolo.zanieri.13@gmail.com>
2021-04-18 20:41:23 +0200

    Added list and detail class based views

--- src/blog/urls.py
+++ src/blog/urls.py
@@ -1,11 +1,15 @@
 from django.contrib import admin
 from django.urls import include, path
 from .views import (
-    articles_create_view
+    articles_create_view,
+    ArticlesListView,
+    ArticlesDetailView
 )
 
 
-app_name = 'blog'
+app_name = 'articles'
 urlpatterns = [
     path('create/', articles_create_view, name="articles-create"),
+    path('articles/', ArticlesListView.as_view(), name="articles-list"),
+    path('articles/<int:id>/', ArticlesDetailView.as_view(), name="articles-detail")
 ]
--- src/blog/views.py
+++ src/blog/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import get_object_or_404, render
-from .forms import CreateArticleForm
+
+
 # Create your views here.
+from .models import Article
+from .forms import CreateArticleForm
+from django.views.generic import (
+    ListView,
+    DetailView
+)
 
 
 def articles_create_view(request):
@@ -11,4 +18,17 @@ def articles_create_view(request):
     context = {
         'form': form
     }
-    return render(request, "blog/articles_create.html", context)
+    return render(request, "articles/articles_create.html", context)
+
+
+class ArticlesListView(ListView):
+    queryset = Article.objects.all()
+    template_name = 'articles/articles_list.html'
+
+
+class ArticlesDetailView(DetailView):
+    template_name = 'articles/articles_detail.html'
+
+    def get_object(self):
+        id_ = self.kwargs.get("id")
+        return get_object_or_404(Article, id=id_)
